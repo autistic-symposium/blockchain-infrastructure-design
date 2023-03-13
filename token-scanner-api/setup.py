161c82ea9f13ca49e06c842124950353ee9fe765
osiris account <osiris137@protonmail.com>
2023-03-12 23:52:15 -0700

    💾

--- token-scanner-api/setup.py
+++ token-scanner-api/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="indexer",
-    version='0.0.3',
+    version='0.0.2',
     packages=find_packages(include=[
         'src', 
         'src.blockchains', 
